c67f9c5550f6405367168603349272148e9b4fef
hf8823 <hiroshigefuruta@yahoo.co.jp>
2025-05-04 20:48:33 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,36 @@
-const CACHE_NAME = 'pdic-cache-v2';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/sw.js',
-  '/dictionary_part1.txt',
-  '/dictionary_part2.txt',
-  '/dictionary_part3.txt',
-  '/dictionary_part4.txt'
+const CACHE_NAME = 'dictionary-cache-v2';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './dictionary_part1.txt',
+  './dictionary_part2.txt',
+  './dictionary_part3.txt',
+  './dictionary_part4.txt',
+  './manifest.json'
 ];
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }))
+    )
   );
 });
 
 self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
-
