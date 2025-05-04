db11a1928c2f0492d5f7f85296fd971dc99053e9
hf8823 <hiroshigefuruta@yahoo.co.jp>
2025-05-04 09:15:27 +0900

    Update index.html and sw.js for multiple dictionary files

--- sw.js
+++ sw.js
@@ -1,19 +1,28 @@
-const CACHE_NAME = 'dict-cache-v1';
+const CACHE_NAME = 'pdic-cache-v2';
 const urlsToCache = [
-  './',
-  './index.html',
-  './RYAKU98U.txt'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/sw.js',
+  '/dictionary_part1.txt',
+  '/dictionary_part2.txt',
+  '/dictionary_part3.txt',
+  '/dictionary_part4.txt'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
   );
 });
 
